114f185ddad0e36f400809d0142cfd160cf52cc6
Danielle Sublett <danielle.sublett@gmail.com>
2014-10-30 11:02:32 -0700

    add masonry and styles

--- app/scripts/collections/big_collection.js
+++ app/scripts/collections/big_collection.js
@@ -1,6 +1,6 @@
 var BigCollection = Backbone.Collection.extend({
   model: Big,
-  url: "https://api.getchute.com/v2/albums/aus6kwrg/assets?type=image&tags=surfing&per_page=20",
+  url: "https://api.getchute.com/v2/albums/aus6kwrg/assets?type=image&tags=surfing&per_page=50",
    parse: function (response) {
       return response.data;
     },
