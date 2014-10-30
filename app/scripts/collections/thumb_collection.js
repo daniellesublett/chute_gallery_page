7be99800fc9c730a9612eaca808eb3ee855c0617
Danielle Sublett <danielle.sublett@gmail.com>
2014-10-30 15:00:24 -0700

    remove superfluous thumbnail model, collection and view

--- app/scripts/collections/thumb_collection.js
+++
@@ -1,7 +0,0 @@
-var ThumbsCollection = Backbone.Collection.extend({
-  model: Thumb,
-  url: "https://api.getchute.com/v2/albums/aus6kwrg/assets?type=image&tags=surfing&per_page=50",
-   parse: function (response) {
-      return response.data;
-    },
-});
--- app/scripts/models/thumb_model.js
+++
@@ -1,7 +0,0 @@
-var Thumb = Backbone.Model.extend({
-  parse:function(response){
-      return{
-      thumb: response.thumbnail
-    }
-  }
-});
