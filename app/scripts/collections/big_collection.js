adace3a58d1d071b6444500164ec1b7feec6e702
Danielle Sublett <danielle.sublett@gmail.com>
2014-11-03 22:58:23 -0800

    Shift logic for parse to collection

--- app/scripts/collections/big_collection.js
+++ app/scripts/collections/big_collection.js
@@ -1,9 +1,14 @@
 var BigCollection = Backbone.Collection.extend({
   model: Big,
   url: "https://api.getchute.com/v2/albums/aus6kwrg/assets?type=image&tags=surfing&per_page=50",
-   parse: function (response) {
-      return response.data;
-    },
+  parse: function (response) {
+    var dataHash = response.data;
+    return _.map(dataHash, function(photoInfo){
+      return {
+        url: photoInfo.url
+      }
+    });
+  },
 });
 
 
--- app/scripts/models/big_model.js
+++ app/scripts/models/big_model.js
@@ -1,7 +1,2 @@
 var Big = Backbone.Model.extend({
-  parse:function(response){
-      return{
-      url: response.url
-    }
-  }
 });
